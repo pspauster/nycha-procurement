d3cdc9fbe22823be00d720775d92b2b3854a850e
Patrick Spauster <patrick.spauster@gmail.com>
2024-02-08 12:45:49 -0500

    created data folder

--- read_data.R
+++ read_data.R
@@ -6,7 +6,7 @@ library(pdftools)
 
 tables_decide <- extract_tables("pdfs/NYCHA_SMALL_PROCUREMENT_2022__Sorted_by_Borough_new.pdf", pages = 1:3)
 
-tables_lattice <- extract_tables("pdfs/NYCHA_SMALL_PROCUREMENT_2022__Sorted_by_Borough_new.pdf", pages = 1:3, method = "lattice")
+tables_lattice <- extract_tables("pdfs/NYCHA_SMALL_PROCUREMENT_2022__Sorted_by_Borough_new.pdf", pages = 1:2, method = "lattice")
 tables_lattice2 <- extract_tables("pdfs/NYCHA_SMALL_PROCUREMENT_2022__Sorted_by_Borough_new.pdf", pages = 4:6, method = "lattice")
 
 get_n_pages("pdfs/NYCHA_SMALL_PROCUREMENT_2022__Sorted_by_Borough_new.pdf")
